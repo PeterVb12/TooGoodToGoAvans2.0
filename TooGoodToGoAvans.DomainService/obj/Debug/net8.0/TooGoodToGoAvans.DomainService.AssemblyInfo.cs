61c339bac3f95948cae402c65c8037fea3ee05d3
PeterVb12 <peter-veter@outlook.com>
2024-10-10 13:50:32 +0200

    general solution structure

--- TooGoodToGoAvans.DomainService/obj/Debug/net8.0/TooGoodToGoAvans.DomainService.AssemblyInfo.cs
+++ TooGoodToGoAvans.DomainService/obj/Debug/net8.0/TooGoodToGoAvans.DomainService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TooGoodToGoAvans.DomainService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+702c0007a4f5fc5869a059d457083b15043e2684")]
 [assembly: System.Reflection.AssemblyProductAttribute("TooGoodToGoAvans.DomainService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TooGoodToGoAvans.DomainService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TooGoodToGoAvans.UI.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TooGoodToGoAvans.UI.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
